4eac99383fb92f59b171ff8b8f761f3bcbeb02a1
Aryasaleh19 <aryasaleh582@gmail.com>
2025-08-27 04:55:47 +0800

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Makanya gantengs</title>
+    <title>Makanya ganteng</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
